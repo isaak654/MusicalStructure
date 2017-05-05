cdad062aa60df6c7b59f9e37ba089b74cc5ae78f
isaak654 <isaak654@users.noreply.github.com>
2017-05-05 14:38:01 +0200

    update1

--- app/src/main/java/com/example/android/musicalstructure/PlaylistActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/PlaylistActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class PlaylistActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class PlaylistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+
+        //Find the View that shows Playlist1 item
+        TextView playlist1 = (TextView) findViewById(R.id.Playlist1);
+
+        if (playlist1 != null) {
+            //Set a click listener on that View
+            playlist1.setOnClickListener(new View.OnClickListener() {
+                //This code will be executed when the playlist1 View is clicked on
+                @Override
+                public void onClick(View view) {
+                    //Create a new intent to open Now Playing Activity
+                    Intent intentPlaylist1 = new Intent(PlaylistActivity.this, NowPlayingActivity.class);
+                    //Start the new activity
+                    startActivity(intentPlaylist1);
+                }
+            });
+        }
+
+        //Find the View that shows Playlist2 item
+        TextView playlist2 = (TextView) findViewById(R.id.Playlist2);
+
+        if (playlist2 != null) {
+            //Set a click listener on that View
+            playlist2.setOnClickListener(new View.OnClickListener() {
+                //This code will be executed when the playlist2 View is clicked on
+                @Override
+                public void onClick(View view) {
+                    //Create a new intent to open Now Playing Activity
+                    Intent intentPlaylist2 = new Intent(PlaylistActivity.this, NowPlayingActivity.class);
+                    //Start the new activity
+                    startActivity(intentPlaylist2);
+                }
+            });
+        }
+
     }
 }
--- app/src/main/java/com/example/android/musicalstructure/SongsActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/SongsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class SongsActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class SongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        //Find the View that shows Song1 item
+        TextView song1 = (TextView) findViewById(R.id.Song1);
+
+        if (song1 != null) {
+            //Set a click listener on that View
+            song1.setOnClickListener(new View.OnClickListener() {
+                //This code will be executed when the song1 View is clicked on
+                @Override
+                public void onClick(View view) {
+                    //Create a new intent to open Now Playing Activity
+                    Intent intentSong1 = new Intent(SongsActivity.this, NowPlayingActivity.class);
+                    //Start the new activity
+                    startActivity(intentSong1);
+                }
+            });
+        }
+
+        //Find the View that shows Song2 item
+        TextView song2 = (TextView) findViewById(R.id.Song2);
+
+        if (song2 != null) {
+            //Set a click listener on that View
+            song2.setOnClickListener(new View.OnClickListener() {
+                //This code will be executed when the song2 View is clicked on
+                @Override
+                public void onClick(View view) {
+                    //Create a new intent to open Now Playing Activity
+                    Intent intentSong2 = new Intent(SongsActivity.this, NowPlayingActivity.class);
+                    //Start the new activity
+                    startActivity(intentSong2);
+                }
+            });
+        }
     }
 }
